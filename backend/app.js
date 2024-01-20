b32cd75dedf1e8cf235c0fb14ad10d92a62d13ed
Ravi Ranjan <2140131@sliet.ac.in>
2024-01-21 01:09:19 +0530

    mongodb connection established

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const connectDb = require('./config/db');
 
 const app = express();
 app.use(express.json());
@@ -8,4 +9,5 @@ app.use("/",require("./routes"));
 
 app.listen("8000",()=>{
     console.log("Server is listening at port 8000")
+    connectDb();
 })
--- backend/controller/authentication.js
+++ backend/controller/authentication.js
@@ -1,8 +1,29 @@
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 module.exports.SignUp = async (req,res)=>{
-    console.log(req.body);
+    const {name, username, email, password } = req.body;
+    console.log(password);
+    bcrypt.hash(password, saltRounds, (err, result)=>{
+        if(!err){
+            console.log(result);
+        }else{
+            res.status(501).json(err);
+        }
+    })
+    res.send("success");
 }
 
 
 module.exports.SignIn = async (req,res)=>{
-    console.log(req.body);
+    const { email , password } = req.body;
+    const hashedPassword = await bcrypt.hash(password,saltRounds);
+    bcrypt.compare(password,hashedPassword,function(err,result){
+        if(!err){
+            console.log(result);
+        }else{
+            res.status(501).json(err);
+        }
+    })
+    res.send("success");
 }
