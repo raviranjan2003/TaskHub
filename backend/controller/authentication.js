4b45fb2704f647dd56a220b37ac04a10556b37d1
Ravi Ranjan <2140131@sliet.ac.in>
2024-01-21 01:46:13 +0530

    authentication completed

--- backend/controller/authentication.js
+++ backend/controller/authentication.js
@@ -1,29 +1,55 @@
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const User = require('../model/users');
 
-module.exports.SignUp = async (req,res)=>{
-    const {name, username, email, password } = req.body;
-    console.log(password);
-    bcrypt.hash(password, saltRounds, (err, result)=>{
-        if(!err){
-            console.log(result);
-        }else{
-            res.status(501).json(err);
+module.exports.SignUp = async (req, res) => {
+    const { name, username, email, password } = req.body;
+    try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(200).json({ message: "Email already registered !" });
         }
-    })
-    res.send("success");
+        bcrypt.hash(password, saltRounds, async (err, result) => {
+            if (!err) {
+                const newUser = new User({
+                    name,
+                    username,
+                    email,
+                    password: result
+                })
+                const newUserDetails = await newUser.save();
+                if (newUserDetails) {
+                    res.status(201).json(newUserDetails);
+                } else {
+                    res.status(501).json({ message: "Error while signUp" })
+                }
+            } else {
+                res.status(501).json(err);
+            }
+        })
+    } catch (error) {
+        res.status(501).json({ error });
+    }
 }
 
 
-module.exports.SignIn = async (req,res)=>{
-    const { email , password } = req.body;
-    const hashedPassword = await bcrypt.hash(password,saltRounds);
-    bcrypt.compare(password,hashedPassword,function(err,result){
-        if(!err){
-            console.log(result);
-        }else{
+module.exports.SignIn = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+        return res.send(200).json({ message: "User not found !" });
+    }
+    bcrypt.compare(password, user.password, function (err, result) {
+        if (!err) {
+            if (result) {
+                console.log("User Logged In !");
+                res.status(200).json({ message: "User Logged In!" });
+            }else{
+                res.status(401).json({ message: "Invalid Credentials!" });
+            }
+        } else {
             res.status(501).json(err);
         }
     })
-    res.send("success");
+    // res.send("success");
 }
