e389e09bf7ff8eff43ab9dc5032eb5e5c349ac4c
Ravi Ranjan <2140131@sliet.ac.in>
2024-01-21 00:04:14 +0530

    authentication controller added

---
+++ backend/controller/authentication.js
@@ -0,0 +1,8 @@
+module.exports.SignUp = async (req,res)=>{
+    console.log(req.body);
+}
+
+
+module.exports.SignIn = async (req,res)=>{
+    console.log(req.body);
+}
--- backend/routes/authentication.js
+++ backend/routes/authentication.js
@@ -3,3 +3,4 @@ const authentication = require('../controller/authentication');
 const app = express();
 
 app.post("/sign-up",authentication.SignUp);
+app.post("/sign-in",authentication.SignIn);
