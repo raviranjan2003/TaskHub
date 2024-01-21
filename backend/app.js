ff476ba209986901bd39cbca458318375ce8fa11
Ravi Ranjan <2140131@sliet.ac.in>
2024-01-21 16:31:17 +0530

    cors added

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const connectDb = require('./config/db');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 
 app.use("/",require("./routes"));
 
