29dda82928ddd504df7d16611adf24148cdcd16c
Ravi Ranjan <2140131@sliet.ac.in>
2024-02-14 18:34:31 +0530

    some changes in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
-import './App.css';
+import { Route, Routes } from 'react-router';
+import Button from '@mui/material/Button';
+import HomeNavbar from './components/HomeNavbar';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        TO DO LIST
-      </header>
+    <div>
+      <HomeNavbar />
+      <Routes>
+        <Route path="/sign-up" element={<SignUp/>} />
+        <Route path="/sign-in" element={<SignIn/>} />
+      </Routes>
     </div>
   );
 }
+
 
 export default App;
