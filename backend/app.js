4c400ad5d3941744febd5cd932d3027530f189a4
raviranjan2003 <2140131@sliet.ac.in>
2024-08-18 01:03:49 +0530

    adding cookie-parser middleware

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const connectDb = require('./config/db');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
 
