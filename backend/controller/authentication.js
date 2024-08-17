4b1280bd22017c49358973abc46760cce314c92b
raviranjan2003 <2140131@sliet.ac.in>
2024-08-18 00:25:54 +0530

    feat: signin more secured(cookie)

--- backend/controller/authentication.js
+++ backend/controller/authentication.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const User = require('../model/users');
 const jwt = require('jsonwebtoken');
-const SECRET_KEY = "Thisismysecretkey";
+// const SECRET_KEY = "Thisismysecretkey";
 
 module.exports.SignUp = async (req, res) => {
     const { name, username, email, password } = req.body;
@@ -47,26 +47,38 @@ module.exports.SignIn = async (req, res) => {
         if (!userDetails) {
             return res.status(200).json({ message: "User not found !" });
         }
-        bcrypt.compare(password, userDetails.password, function (err, result) {
-            if (!err) {
-                const token = jwt.sign({id:userDetails._id, email:userDetails.email},SECRET_KEY);
-                const user = {
-                    _id : userDetails._id,
-                    username : userDetails.username,
-                    name : userDetails.name,
-                    email : userDetails.email,
-                    token
-                }
-                if (result) {
-                    res.status(200).json({ message: "User Logged In!", user });
-                }else{
-                    res.status(401).json({ message: "Invalid Credentials!" });
-                }
-            } else {
-                res.status(501).json(err);
-            }
-        })
+        // bcrypt.compare(password, userDetails.password, function (err, result) {
+        //     if (!err) {
+        //         const token = jwt.sign({id:userDetails._id, email:userDetails.email},SECRET_KEY);
+        //         const user = {
+        //             _id : userDetails._id,
+        //             username : userDetails.username,
+        //             name : userDetails.name,
+        //             email : userDetails.email,
+        //             token
+        //         }
+        //         if (result) {
+        //             res.status(200).json({ message: "User Logged In!", user });
+        //         }else{
+        //             res.status(401).json({ message: "Invalid Credentials!" });
+        //         }
+        //     } else {
+        //         res.status(501).json(err);
+        //     }
+        // })
+        userDetails.password = null;
+        const token = jwt.sign({user : userDetails }, process.env.SECRET_KEY, { expiresIn: '1h' });
+        
+        res.cookie('token', token, {
+          httpOnly: true, // Prevents JavaScript access to the cookie
+        //   secure: process.env.NODE_ENV === 'production', // Set to true in production
+          sameSite: 'Strict', // Helps prevent CSRF attacks
+        });
+
+        res.status(200).json({ message : "Login Successfully !"});
+
     } catch (error) {
+        console.log(error);
         res.status(500).json({ message : "Something Went Wrong !"});
     }
     // res.send("success");
