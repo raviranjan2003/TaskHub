2d52574390d01617bfd462c3cf1c22eeb260329f
raviranjan2003 <2140131@sliet.ac.in>
2024-08-17 23:22:16 +0530

    mongo-uri secured

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDb = () => {
     mongoose
-    .connect("mongodb+srv://ravidemo3:Ravi2003@cluster0.drwb5mc.mongodb.net/?retryWrites=true&w=majority")
+    .connect(process.env.MONGO_URI)
     .then(()=>{
         console.log("MongoDB connected !");
     })
